f9767f6759767e6844984c7c255ff55e39dbc518
Artem Kornienko <send.this.to.moff@gmail.com>
2016-11-06 14:46:30 +0200

    Fix

--- Lyra2.c
+++ Lyra2.c
@@ -209,7 +209,7 @@ int LYRA2(void *K, uint64_t kLen, const void *pwd, uint64_t pwdlen, const void *
     free(state);
     //==========================================================================/
 
-    return 0;
+    /*return 0;*/
 }
 
 int LYRA2_old(void *K, uint64_t kLen, const void *pwd, uint64_t pwdlen, const void *salt, uint64_t saltlen, uint64_t timeCost, uint64_t nRows, uint64_t nCols) {
